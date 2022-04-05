72813f311e93ee2a3dfad56a708dcf04678befd0
Tania <ts2204@mail.ru>
2022-04-05 22:56:19 +0700

    Проектная работа 4(2)

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,27 +1,30 @@
 const aboutProjectLink = document.querySelector('.profile__info-edit')
 const modalWindow = document.querySelector('.popup');
 const modalClose = modalWindow.querySelector('.popup__close');
-const nameInput = document.querySelector('.popup__name');
+const nameInput = document.querySelector('.popup__element_name');
 const profileNameLink = document.querySelector('.profile__name');
-const professionInput = document.querySelector('.popup__profession');
+const professionInput = document.querySelector('.popup__element_profession');
 const profileProfessionLink = document.querySelector('.profile__profession');
 const myForm = document.querySelector('.popup__content');
 
-function toggleModalWindow() {
-    modalWindow.classList.toggle('popup_opened-close');
+function openModalWindow() {
+    modalWindow.classList.add('popup_opened');
     nameInput.value = profileNameLink.textContent;
     professionInput.value = profileProfessionLink.textContent;
 }
+aboutProjectLink.addEventListener('click', openModalWindow);
 
-aboutProjectLink.addEventListener('click', toggleModalWindow);
+function closeModalWindow() {
+    modalWindow.classList.remove('popup_opened');
+}
 
-modalClose.addEventListener('click', toggleModalWindow);
+modalClose.addEventListener('click', closeModalWindow);
 
 function onSubmit(e) {
     e.preventDefault();
     profileNameLink.textContent = nameInput.value;
     profileProfessionLink.textContent = professionInput.value;
-    toggleModalWindow();
+    closeModalWindow();
 }
 
 myForm.addEventListener('submit', onSubmit);
