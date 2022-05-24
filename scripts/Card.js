0facdbeea81a9f1da2ec8512d8f45e9ed4a74706
Tania <ts2204@mail.ru>
2022-05-25 00:15:22 +0700

    Проектная работа 7(1)

---
+++ scripts/Card.js
@@ -0,0 +1,76 @@
+import {openPopup, closePopup,  modalPictures, locationImage,
+    viewImageClose, locationName, closeOverlayClick} from './utils.js';
+
+class Card {
+    constructor(data, cardSelector) {
+        this._name = data.name;
+        this._link = data.link;
+        this._cardSelector = cardSelector;
+    }
+
+    _getTemplate = () => {
+        const cardElement = document
+            .querySelector(this._cardSelector)
+            .content
+            .querySelector('.element')
+            .cloneNode(true);
+
+        return cardElement;
+    }
+
+    generateCard = () => {
+        this._element = this._getTemplate();
+        this._setEventListeners();
+
+        this._element.querySelector('.element__mask-group').src = `${this._link}`;
+        this._element.querySelector('.element__location').textContent = this._name;
+        this._element.querySelector('.element__mask-group').alt = this._name;
+
+        this._removePicture = this._element.querySelector('.element__remove');
+        this._removePicture.addEventListener('click', () => {
+            this._removeButtonPicture();
+        });
+        this._likePicture = this._element.querySelector('.element__like');
+        this._likePicture.addEventListener('click', () => {
+            this._addLikePicture();
+        });
+        return this._element;
+    }
+
+    _removeButtonPicture = () => {
+        this._element.remove();
+    }
+
+    _addLikePicture = () => {
+        this._likePicture.classList.toggle('element__like_active');
+    }
+
+    _handlePreviewPicture = () => {
+        locationImage.src = this._link;
+        locationName.textContent = this._name;
+        locationImage.alt = this._name;
+        openPopup(modalPictures);
+    }
+
+    _handleClosePicture = () => {
+        locationImage.src = '';
+        locationName.textContent = '';
+        locationImage.alt = '';
+        closePopup(modalPictures);
+    }
+
+    _setEventListeners = () => {
+        this._image = this._element.querySelector('.element__mask-group');
+        this._image.addEventListener('click', () => {
+            this._handlePreviewPicture();
+        });
+
+        viewImageClose.addEventListener('click', () => {
+            this._handleClosePicture();
+        });
+
+        modalPictures.addEventListener('click', closeOverlayClick);
+    }
+}
+
+export {Card};
--- scripts/utils.js
+++ scripts/utils.js
@@ -24,3 +24,37 @@ const initialCards = [
         link: 'https://images.unsplash.com/photo-1605516092650-b5a22b75f966?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
     }
 ];
+
+const modalPictures = document.querySelector('.popup_mask-group');
+const locationImage = document.querySelector('.popup__mask-image');
+const viewImageClose = document.querySelector('.popup__close_image');
+const locationName = document.querySelector('.popup__location-name');
+
+const numberEscape = 27;
+
+function openPopup(modalPictures) {
+    modalPictures.classList.add('popup_opened');
+    document.addEventListener('keydown', closeEscape);
+}
+
+function closePopup(modalPictures) {
+    modalPictures.classList.remove('popup_opened');
+    document.removeEventListener('keydown', closeEscape);
+}
+
+function closeEscape(event) {
+    if (event.keyCode === numberEscape) {
+        const popupActive = document.querySelector('.popup_opened');
+        closePopup(popupActive);
+    }
+}
+
+function closeOverlayClick(event) {
+    if (event.target === event.currentTarget) {
+        const popupActive = document.querySelector('.popup_opened');
+        closePopup(popupActive);
+    }
+}
+
+
+export {initialCards, openPopup, closePopup, modalPictures, locationImage, viewImageClose, locationName, closeOverlayClick};
